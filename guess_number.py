750059c94ba5a4a2183544189b35c64e44236932
Lucas Hinkle <lwhinkle@memphis.edu>
2024-02-07 23:35:22 -0600

    Added a loop that lets the user keep guessing numbers until correct or until they press q

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,17 @@
 number = 10
+keep_going = True
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+while keep_going:
+    print("I'm thinking of a number...")
+    guess = input("What number am I thinking of? ")
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+    if guess != 'q':
+        guess = int(guess)
+
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+    elif guess == 'q':
+        keep_going = False
+        print(f"The number was {number}.")
+    else:
+        print(f"Sorry! That's incorrect.")
